6d8892b5e4d144f7cfbd2aabcaeccd673c20e4d5
Kyrylo Chernyshov <kc875@cornell.edu>
2018-05-09 19:20:37 -0400

    Calculate dimensions properly

--- src/reducers/dimensions.js
+++ src/reducers/dimensions.js
@@ -1,6 +1,9 @@
+// map dimensions: 2185 x 1743
+import config from '../config.json';
+
 const initial = {
-    width: 500,
-    height: 300
+    width: 2185 / config.scale,
+    height: 1743 / config.scale
 };
 
 export default (state = initial, action) => {
