fe1c0f2af559e3b313b7c6d8fd59a42b5e7fab3a
Kyrylo Chernyshov <kc875@cornell.edu>
2018-05-09 22:17:18 -0400

    Didn't I commit this earlier today

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import dimensionsReducer from './dimensions';
+import locationsReducer from './locations';
 
 const rootReducer = combineReducers({
-    dimensions: dimensionsReducer
+    dimensions: dimensionsReducer,
+    locations: locationsReducer
 });
 
 export default rootReducer;
