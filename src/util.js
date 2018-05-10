df068f0f03ad798b9ab1330d2146085cb19709dd
Kyrylo Chernyshov <kc875@cornell.edu>
2018-05-09 22:11:53 -0400

    Add state to object conversion function, with inverse

--- src/util.js
+++ src/util.js
@@ -11,3 +11,39 @@ export const listToArray = list => {
 
     return listToArrayStep(list, []);
 };
+
+const stateToObjMap = [
+    'player_index',
+    'players',
+    'routes',
+    'destination_deck',
+    'destination_trash',
+    'choose_destinations',
+    'train_deck',
+    'facing_up_trains',
+    'train_trash',
+    'taking_routes',
+    'error',
+    'turn_ended'
+];
+
+// Convert OCaml State to JS State Object
+export const stateToObj = state => {
+    return stateToObjMap.reduce(
+        (acc, elem, index) => {
+            acc[elem] = state[index];
+            return acc;
+        },
+        {}
+    );
+};
+
+// Convert JS State Object to OCaml State
+export const objToState = obj => {
+    const state = [];
+    for(const [key, value] of Object.entries(obj)) {
+        const i = stateToObjMap.indexOf(key);
+        state[i] = value;
+    }
+    return state;
+};
