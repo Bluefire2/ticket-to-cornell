c3e599e9355e3f51bc53f91931d6bc073f351dfd
Kyrylo Chernyshov <kc875@cornell.edu>
2018-05-10 23:01:32 -0400

    Add skeleton code for player details in the top pane

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hands from './containers/hands';
+import PlayerDetails from './containers/player_details';
 
 import Map from './containers/map';
 
@@ -9,6 +10,9 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+                <div id="top-pane">
+                    <PlayerDetails/>
+                </div>
                 <div id="map-container">
                     <Map/>
                 </div>
