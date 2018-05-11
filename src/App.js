72583712bd18dd209472a0a87ec85151013bedf3
Kyrylo Chernyshov <kc875@cornell.edu>
2018-05-11 13:56:57 -0400

    Draw destination ticket card

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Hands from './containers/hands';
 import PlayerDetails from './containers/player_details';
+import DestinationTicket from './components/destination_ticket';
 
 import Map from './containers/map';
 
@@ -17,6 +18,7 @@ class App extends Component {
                     <Map/>
                 </div>
                 <div id="bottom-pane">
+                    <DestinationTicket from="Becker House" to="Comstock Hall" points={15} />
                     <Hands/>
                 </div>
             </div>
--- src/components/destination_ticket.js
+++ src/components/destination_ticket.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 
-export default ({from, to, points}) => {
+export default ({from, to, points, completed}) => {
+    const pointsClassName = `destination-ticket-points ${completed ? 'completed' : ''}`;
     return (
         <div className="destination-ticket">
             <div className="destination-ticket-top">{from}</div>
             <div className="destination-ticket-bottom">{to}</div>
-            <div className="destination-ticket-points">{points}</div>
+            <div className={pointsClassName}>{points}</div>
         </div>
     );
 };
