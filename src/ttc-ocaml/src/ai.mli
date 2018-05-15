5f3b0cbf3e031ea9b93e59cdb9ceed7bfd30d65b
Camila Pretiz <camilapretiz@gmail.com>
2018-05-15 12:03:17 -0400

    Add AI specs

--- src/ttc-ocaml/src/ai.mli
+++ src/ttc-ocaml/src/ai.mli
@@ -1,17 +1,23 @@
 open Components
 
+(* [action] represents the next possible action the AI can do. *)
 type action = Take_DTicket | Place_Train | Take_Faceup | Take_Deck
 
+(* [next_move lst td p] is the next action the AI wants to do depending on the
+ * current player [p], the facing up cards [td], and the routes [lst]. *)
 val next_move : Board.route list -> TrainDeck.t -> Player.player -> action
 
-(* Given the facing up cards list, choose which one the AI wants. *)
+(* [ai_facing_up p td] is the facing up card index the AI wants to choose from
+ * the facing up cards [td]. *)
 val ai_facing_up : Player.player -> TrainDeck.t -> int
 
-(* Given the player, the route list, and the choose destinations, choose which one/s the AI wants. *)
+(* [ai_take_dticket p lst td] is the list of indeces the AI wants to choose from
+ * the list of destination tickets [lst]. *)
 val ai_take_dticket : Player.player -> Board.route list -> DestinationDeck.card list -> int list
 
-(* Gives me the route you want to place, the train color that you want to use to
- * place these trains and the number of wild cards that you want to use for this. *)
+(* [ai_place_train p lst] is a tuple with which route the AI wants to choose,
+ * with which tarin card color they want to fill in the route, and how many
+ * wildcards they want to use to place the train. *)
 val ai_place_train : Player.player -> Board.route list -> Board.route * Components.train_color * int
 
 (* [ai_setup p lst] is the list of indeces of the cards the AI wants to keep from
