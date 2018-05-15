213d488d02e79ac12099ba6f95198fc4b42709a7
Kyrylo Chernyshov <kc875@cornell.edu>
2018-05-15 14:45:12 -0400

    Add n_bots to the query string

--- src/reducers/INIT_STATE.js
+++ src/reducers/INIT_STATE.js
@@ -2,7 +2,8 @@ import {init_state, setup_state, end_state1, end_state2, end_state3} from '../tt
 import {stateToObj} from '../util';
 
 const url = new URL(window.location.href);
-let nPlayers = url.searchParams.get("n_players");
+let nPlayers = url.searchParams.get("n_players"),
+    nBots = url.searchParams.get("n_bots");
 
 if(nPlayers === null) {
     nPlayers = 2;
@@ -10,5 +11,11 @@ if(nPlayers === null) {
     nPlayers = parseInt(nPlayers);
 }
 
-export default stateToObj(setup_state(init_state(nPlayers, 0)));
+if(nBots === null) {
+    nBots = 2;
+} else {
+    nBots = parseInt(nBots);
+}
+
+export default stateToObj(setup_state(init_state(nPlayers, nBots)));
 // export default stateToObj(end_state3);
